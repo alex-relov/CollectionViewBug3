ea18b2c1abfb2885a0fa276b50afed83a28559a3
alex <info@alex-relov.ru>
2020-11-15 13:39:42 +0500

    Add project files.

---
+++ CollectionViewBug3/CollectionViewBug3/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CollectionViewBug3
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Grouping<string, string>> Items { get; set; } = new ObservableCollection<Grouping<string, string>>()
+        {
+            new Grouping<string, string>("Grope 1",new List<string>())
+        };
+
+        public ICommand AddRow { get; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            AddRow = new Command<Grouping<string, string>>((arg) =>
+           {
+               arg.Add($"{arg.Count + 1}");
+           });
+        }
+    }
+}
